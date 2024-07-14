6e5a4c12ce61645a1a96c8320d0887fd5900f9de
“Snappyhacker” <jimodhiambo515@gmail.com>
2024-07-14 10:04:06 +0300

    liker

--- main.js
+++ main.js
@@ -1,11 +1,41 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  const hearts = document.querySelectorAll(".like-glyph");
 
+  hearts.forEach(heart => {
+    heart.addEventListener("click", () => {
+      handleHeartClick(heart);
+    });
+  });
+});
 
+function handleHeartClick(heart) {
+  mimicServerCall()
+    .then(() => {
+      if (heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+        heart.classList.add("activated-heart");
+      } else {
+        heart.innerText = EMPTY_HEART;
+        heart.classList.remove("activated-heart");
+      }
+    })
+    .catch((error) => {
+      const modal = document.getElementById("modal");
+      const modalMessage = document.getElementById("modal-message");
 
+      modalMessage.innerText = error;
+      modal.classList.remove("hidden");
+
+      setTimeout(() => {
+        modal.classList.add("hidden");
+      }, 3000);
+    });
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
